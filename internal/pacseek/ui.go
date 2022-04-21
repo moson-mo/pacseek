f891f35411df2c0457b0dfe662c257de0c4f92ba
moson-mo <moson-mo@users.noreply.github.com>
2022-04-21 17:23:38 +0200

    update: bump to v1.2.6

--- internal/pacseek/ui.go
+++ internal/pacseek/ui.go
@@ -17,7 +17,7 @@ const (
 	aurPackageUrl  = "https://aur.archlinux.org/packages/"
 	packageUrl     = "https://archlinux.org/packages/"
 
-	version = "1.2.5"
+	version = "1.2.6"
 )
 
 var archRepos = []string{"core", "community", "community-testing", "extra", "kde-unstable", "multilib", "multilib-testing", "testing"}
