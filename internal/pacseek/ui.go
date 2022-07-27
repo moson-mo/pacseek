36bb4b44be2ff9a6ada1c173d432f1d520242fdd
moson-mo <mo-son@mailbox.org>
2022-07-27 21:02:33 +0200

    update: bump to v1.4.4
    Signed-off-by: moson-mo <mo-son@mailbox.org>

--- internal/pacseek/ui.go
+++ internal/pacseek/ui.go
@@ -15,7 +15,7 @@ const (
 	aurPackageUrl = "https://aur.archlinux.org/packages/"
 	packageUrl    = "https://archlinux.org/packages/"
 
-	version = "1.4.3"
+	version = "1.4.4"
 )
 
 // UI is holding our application information and all tview components
