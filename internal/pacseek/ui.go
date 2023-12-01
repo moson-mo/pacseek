a0c0d8130d447ded8a2f0c72ecee8c71fdd31f60
moson-mo <mo-son@mailbox.org>
2023-12-01 14:21:44 +0100

    update: bump to v1.8.1
    Signed-off-by: moson-mo <mo-son@mailbox.org>

--- internal/pacseek/ui.go
+++ internal/pacseek/ui.go
@@ -23,7 +23,7 @@ const (
 
 	UrlAurMaintainer = "https://aur.archlinux.org/packages?SeB=m&K=%s"
 
-	version = "1.8.0"
+	version = "1.8.1"
 )
 
 // UI is holding our application information and all tview components
